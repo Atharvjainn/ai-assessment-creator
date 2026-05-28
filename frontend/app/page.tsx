2604b7ad2cbd40bef2c32891b7bef95078e061d5
Atharv Jain <jainatharv20@gmail.com>
2026-05-28 23:58:58 +0530

    fixing

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,8 +2,11 @@ import EmptyAssignment from '@/components/EmptyAssignment'
 import Sidebar from '@/components/SideBar'
 import TopNavbar from '@/components/TopNavbar'
 import React from 'react'
+import { useRouter } from 'next/navigation'
 
 const page = () => {
+  const router = useRouter()
+  router.push('/dashboard')
   return (
     <>
      <div>Hi</div>

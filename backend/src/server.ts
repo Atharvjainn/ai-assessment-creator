cd270d001836088e874ec85f2dcf0b087148588a
Atharv Jain <jainatharv20@gmail.com>
2026-05-28 21:25:56 +0530

    cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,10 @@ import cors from 'cors'
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+    origin : "http://localhost:3000",
+    credentials : true
+}));
 
 app.use(express.json());
 
